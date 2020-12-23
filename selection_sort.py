4b8d4211d36e5f919e9e7d3895caacff4e45989e
Montukeshwar Vaishnaw <46618399+montukv@users.noreply.github.com>
2020-12-23 10:25:19 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -4,7 +4,7 @@ def selectionSort(s):
 		for j in range(i,len(s)):
 			if s[cur_index] > s[j]:
 				cur_index = j
-		s[i],s[cur_index] = s[cur_index],s[i]
+		s[i],s[cur_index] =  s[cur_index],s[i]
 	return s
 s = [3,2,1,5,4]
 print(selectionSort(s))
